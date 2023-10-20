2bb26b71239f668c34ae7ad176ea5424a9296feb
MandyyChen <mandychen022@gmail.com>
2023-10-20 10:03:24 -0400

    10/20

--- index.js
+++ index.js
@@ -87,14 +87,29 @@ const titles = [
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
 books.forEach((author)=> {
-  console.log($(author.authorFirst) + " " + author.authorLast + " " + "wrote" + " " + (author.name) + " " + "in" + " " + (author.publishDate));
+  console.log(`${author.authorFirst} ${author.authorLast} wrote ${author.name} in ${author.publishDate}`);
 })
 
 //Sort books from oldest to most recent
 
 //sort books alphabetically
+books.sort((a, b) => {
+  const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  // names must be equal
+  return 0;
+});
+books.forEach((book) => console.log(book.name));
 
 //Find who wrote War and Peace
+const war = books.filter((author) => author.name.includes("War and Peace"));
+console.log(war);
 
 //how many books were written before 1900?
 
@@ -103,3 +118,5 @@ books.forEach((author)=> {
 //was every book published within the last 100 years?
 
 //print a list of books that "includes" the genre historical
+const history = books.filter((author) => author.genre.includes("historical"));
+console.log(history);
